ad80bd00d3b4cbfdd0c9359052183f8fa9793d5a
Zach Stearman <zstearman3@gmail.com>
2020-08-01 09:34:30 -0500

    Updates

---
+++ precache-manifest.7c814e35d2f22f6c3aeebd5680651d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7012753021ae16d6e9884434ecf094",
+    "url": "/react-nba-scoreboard/index.html"
+  },
+  {
+    "revision": "0b70e1f217093938b360",
+    "url": "/react-nba-scoreboard/static/css/main.49fcad5b.chunk.css"
+  },
+  {
+    "revision": "8edbca94878f3c90c2f0",
+    "url": "/react-nba-scoreboard/static/js/2.5f7e5024.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-nba-scoreboard/static/js/2.5f7e5024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b70e1f217093938b360",
+    "url": "/react-nba-scoreboard/static/js/main.1565e79c.chunk.js"
+  },
+  {
+    "revision": "9749f7ee33dc21675fe7",
+    "url": "/react-nba-scoreboard/static/js/runtime-main.c24b7339.js"
+  }
+]);
