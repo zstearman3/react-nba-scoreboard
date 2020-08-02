262117da1ad5e36c8da050ab7d9d43ae13e78de7
Zach Stearman <zstearman3@gmail.com>
2020-08-01 21:45:36 -0500

    Add some mobile light mobile styling

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -17,10 +17,14 @@ class GameHeader extends PureComponent {
         <svg height="40" width="40">
           <polygon points="20,0 8,40 40,16 0,16 32,40" />
         </svg>
-        <h3 className="away-team"><strong>{awayTeam}</strong> {awayNickname}</h3>
-        <h4>{awayScore}</h4>
-        <h3 className="home-team"><strong>{homeTeam}</strong> {homeNickname}</h3>
-        <h4>{homeScore}</h4>
+        <div className="away-team-div">
+          <h3 className="away-team"><strong>{awayTeam}</strong> {awayNickname}</h3>
+          <h4>{awayScore}</h4>
+        </div>
+        <div className="home-team-div">
+          <h3 className="home-team"><strong>{homeTeam}</strong> {homeNickname}</h3>
+          <h4>{homeScore}</h4>
+        </div>
         <button className="expand-button" onClick={() => handleExpand()}>
           {isExpanded ? "Hide Details" : "Show Details"}
         </button>
